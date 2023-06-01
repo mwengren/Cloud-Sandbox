204f11ff9cd2e4960d8710011b9f3dfc3debfa3b
Micah Wengren <micah.wengren@gmail.com>
2023-06-01 23:13:39 +0000

    Initial mods to Subnet/Head Node config.  Currently:
    * allows creation of Subnet from existing VPC
    * automatically creates new aws_internet_gateway, route_table, and route_table_association
    * eliminates duplicate aws_network_interface for EFA (only create one and vary type based on 'use_efa' variable)
    * deploys aws_instance.head_node with AWS EIP

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,10 +78,18 @@ data "aws_vpc" "pre-provisioned" {
 resource "aws_subnet" "main" {
    count = var.subnet_id != null ? 0 : 1
    vpc_id = local.vpc.id
-   # This subnet will allow 256 IPs
-   cidr_block = "10.0.0.0/24"
-   map_public_ip_on_launch = true
+
+   # If a subnet_cidr variable is passed explicitly, we use that,  
+   # otherwise, per https://developer.hashicorp.com/terraform/language/functions/cidrsubnet, divide the 
+   # VPC address space in 4 (via 'newbits' value of 2), and
+   # use the first range of address space for the subnet (0 for netnum value) 
+   cidr_block = var.subnet_cidr != null ? var.subnet_cidr : cidrsubnet(one(data.aws_vpc.pre-provisioned[*]).cidr_block, 2, 0)
+   
+   # for the moment, this will vary based on var.head_node_ip presence:
+   map_public_ip_on_launch = var.head_node_ip == null ? true : false
+   
    availability_zone = var.availability_zone
+   
    tags = {
       Name = "${var.name_tag} Subnet"
       Project = var.project_tag
@@ -106,7 +114,8 @@ locals {
 
 
 resource "aws_internet_gateway" "gw" {
-   count = var.vpc_id != null ? 0 : 1
+   # just create one for now to test:
+   # count = var.vpc_id != null ? 0 : 1
    vpc_id = local.vpc.id
    tags = {
       Name = "${var.name_tag} Internet Gateway"
@@ -115,7 +124,9 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_route_table" "default" {
-   count = var.vpc_id != null ? 0 : 1
+   # count = var.vpc_id != null ? 0 : 1
+   # just create one for now to test:
+   # count = local.vpc.main_route_table_id == null ? 1 : 0
    vpc_id = local.vpc.id
 
    route {
@@ -129,7 +140,8 @@ resource "aws_route_table" "default" {
 }
 
 resource "aws_route_table_association" "main" {
-  count = var.vpc_id != null ? 0 : 1
+  # just create one for now to test:
+  # count = var.vpc_id != null ? 0 : 1
   subnet_id = one(aws_subnet.main[*].id)
   route_table_id = one(aws_route_table.default[*].id)
 }
@@ -368,7 +380,8 @@ resource "aws_instance" "head_node" {
   # associate_public_ip_address = true
   network_interface {
     device_index = 0    # MUST be 0
-    network_interface_id = var.use_efa == true ? aws_network_interface.efa_network_adapter.id : aws_network_interface.standard.id
+    # network_interface_id = var.use_efa == true ? aws_network_interface.efa_network_adapter.id : aws_network_interface.standard.id
+    network_interface_id = aws_network_interface.head_node.id
   }
 
   # This logic isn't perfect since some ena instance types can be in a placement group also
@@ -404,31 +417,36 @@ data "template_file" "init_instance" {
 }
 
 # Can only attach efa adaptor to a stopped instance!
-resource "aws_network_interface" "standard" {
+resource "aws_network_interface" "head_node" {
   
   subnet_id   = local.subnet.id
-  description = "The network adaptor to attach to the instance if EFA is not supported"
+  description = "The network adaptor to attach to the head_node instance"
   security_groups = [aws_security_group.base_sg.id,
                      aws_security_group.ssh_ingress.id,
                      aws_security_group.efs_sg.id]
+  
+  interface_type = var.use_efa == true ? "efa" : null 
+
   tags = {
-      Name = "${var.name_tag} Standard Network Adapter"
+      Name = "${var.name_tag} Head Node Network Adapter"
       Project = var.project_tag
   }
+
+
+  #private_ips = [var.head_node_ip]
+  #private_ips = [var.head_node_ip]
+
+  # handle here or in instance create or via aws_network_interface_attachment?
+#  attachment {
+#    instance     = aws_instance.head_node.id
+#    device_index = 1
+#  }
+
 }
 
-# Can only attach efa adaptor to a stopped instance!
-resource "aws_network_interface" "efa_network_adapter" {
-  
-  subnet_id   = local.subnet.id
-  description = "The Elastic Fabric Adapter to attach to instance if supported"
-  security_groups = [aws_security_group.base_sg.id,
-                     aws_security_group.ssh_ingress.id,
-                     aws_security_group.efs_sg.id]
-  
-  interface_type = "efa"
-  tags = {
-      Name = "${var.name_tag} EFA Network Adapter"
-      Project = var.project_tag
-  }
-}
+#resource "aws_network_interface_attachment" "head_node" {
+#  instance_id          = aws_instance.head_node.id
+#  network_interface_id = aws_network_interface.head_node.id
+#  device_index         = 0
+#}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -79,7 +79,7 @@ variable "vpc_id" {
 
 
 variable "subnet_id" {
-  description = "The ID of an existing Subnect within the target VPC.  Must be specified if using an exsiting VPC."
+  description = "The ID of an existing Subnet within the target VPC to use.  If not provided a Subnet will be created."
   type = string
   default = null
 
@@ -90,6 +90,31 @@ variable "subnet_id" {
 
 }
 
+variable "subnet_cidr" {
+  description = "An explicit CIDR block to use for the created Subnet, if subnet_id is not specified.  Optional."
+  type = string
+  default = null
+
+  validation {
+    condition     = var.subnet_cidr == null ? true : can(cidrhost(var.subnet_cidr, 0))
+    error_message = "The subnet_cidr variable must be a valid CIDR notation (e.g. \"10.0.0.0/24\")."
+  }
+
+}
+
+
+variable "head_node_ip" {
+  description = "The IP address to assign to the network interface for the head node instance.  Should be from the VPC's publicly-accessible CIDR range."
+  type = string
+  default = null
+  nullable = true
+
+  #validation {
+  #  condition     = var.head_node_ip == null ? true : can(cidrhost(var.head_node_ip, 0))
+  #  error_message = "The head_node_ip variable must be a valid IP address (e.g. \"137.75.95.224\")."
+  #}
+
+}
 
 variable "managed_policies" {
   description = "The attached IAM policies granting machine permissions"
